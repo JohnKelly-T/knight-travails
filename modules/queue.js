c5f07e7b14c1bd07f3d1677db62178eee31a9b82
john-kelly <johnkellytemena@gmail.com>
2025-05-26 17:33:35 +0800

    Create queue class

---
+++ modules/queue.js
@@ -0,0 +1,39 @@
+import { QNode } from "./q-node.js";
+
+export class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  enqueue(value) {
+    let node = new QNode(value);
+
+    if (this.head === null) {
+      this.head = node;
+      this.tail = node;
+      return;
+    }
+
+    this.tail.next = node;
+    this.tail = node;
+  }
+
+  dequeue() {
+    if (this.head === null) throw new Error("Nothing to dequeue");
+
+
+    let node = this.head;
+
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+
+      return node.data;
+    }
+
+    this.head = this.head.next;
+
+    return node.data;
+  }
+}
